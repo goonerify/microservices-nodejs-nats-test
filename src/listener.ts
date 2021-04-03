7940001f5e4e0ae3a4c70ef826e0979f42b742d4
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-03 14:29:43 -0400

    278. Accessing Event Data

--- src/listener.ts
+++ src/listener.ts
@@ -1,17 +1,21 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from "node-nats-streaming";
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
-  url: 'http://localhost:4222',
+const stan = nats.connect("ticketing", "123", {
+  url: "http://localhost:4222",
 });
 
-stan.on('connect', () => {
-  console.log('Listener connected to NATS');
+stan.on("connect", () => {
+  console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe('ticket:created');
+  const subscription = stan.subscribe("ticket:created");
 
-  subscription.on('message', (msg) => {
-    console.log('Message recieved');
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
   });
 });
