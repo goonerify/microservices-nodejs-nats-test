af97caf2c290670752f776e71ef342eceec803a4
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-04 20:47:00 -0400

    290. Durable Subscriptions

--- src/listener.ts
+++ src/listener.ts
@@ -19,12 +19,15 @@ stan.on("connect", () => {
   const options = stan
     .subscriptionOptions()
     .setManualAckMode(true)
-    .setDeliverAllAvailable(); // Get a list of all events ever emitted
+    // Get a list of all events ever emitted. This will only be used the first time a service goes online if we also set durable name
+    .setDeliverAllAvailable()
+    // Ensure that our services only receive events that they haven't processed already
+    .setDurableName("accounting-service"); // Durable name is usually the same as queue group name
 
   const subscription = stan.subscribe(
     "ticket:created",
     // Avoid sending the same message to duplicated services by placing them in the same queue group
-    // "order-service-queue-group",
+    "order-service-queue-group", // Also ensures that durable name persists a disconnection to the NATS server
     options
   );
 
@@ -43,3 +46,4 @@ stan.on("connect", () => {
 // These events 'SIGINT', 'SIGTERM' might be a little finicky on windows
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+process.on("exit", () => stan.close());
