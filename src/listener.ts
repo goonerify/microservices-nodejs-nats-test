d6c0b6707678a601db134dfe0319b4de9ab8056e
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-05 01:03:48 -0400

    310. NATS Client Singleton

--- src/listener.ts
+++ src/listener.ts
@@ -11,7 +11,10 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  // Prevent sending of events once the server goes offline
+  // Prevent sending of events once this NATS streaming server client goes offline
+  // This will theoretically help NATS streaming server to understand that it
+  // should spin up a new client right away and not send heartbeat checks to see
+  // if this old one will restart, which should prevent message delivery delays
   stan.on("close", () => {
     console.log("NATS connection closed");
     process.exit();
